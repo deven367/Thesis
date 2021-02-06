63a4bb65499cd96783f1648c426e9a8b03a6359f
deven367 <masterdeven@gmail.com>
2021-02-06 13:51:49 -0500

    add show in plot_heatmap

--- functions.py
+++ functions.py
@@ -26,6 +26,7 @@ import matplotlib.pyplot as plt
 def plot_heatmap(embeddings):
     cmp = newcmp(0,0,256)
     sns.heatmap(cosine_similarity(embeddings, embeddings) ,square=True, cmap=cmp, vmin=0, vmax = 1)
+    plt.show()
     inp = input('Do you have want to save the figure? ')
     if inp == 'y' or inp == 'Y':
         plt.savefig('figure.png', dpi = 300)
