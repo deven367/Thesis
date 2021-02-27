512da10684460cfdc280c612a14179c17ce6d722
Deven <masterdeven@gmail.com>
2021-02-27 18:06:41 -0500

    generate bulk heatmaps
    Former-commit-id: d60924fb712ab0e29cd124f17c64b29387b1f2cc [formerly 6f55e509941f555e371eab2c39b34f2bea3611d6]
    Former-commit-id: 123fa79caca530470c2a6ca72a52dabad899d8da

--- check_embed.py
+++ check_embed.py
@@ -1,9 +1,20 @@
 import numpy as np
 from functions import read_json, plot_heatmap
+import os
+
 # embeddings =  np.load('ulysses_infersent_glove.npy')
 # print(embeddings[0].shape)
 
 
 embeddings = read_json('gg_embeddings.jsonl')
 # print(embeddings.shape)
-plot_heatmap(embeddings)
+# plot_heatmap(embeddings)
+
+embedding_path = './declutr/embeddings/small/poems/'
+
+for fx in os.listdir(embedding_path):
+    if fx.endswith('.npy'):
+        name = fx[:-4]
+        print("Loaded "+fx)
+        embed = np.load(embedding_path+fx)
+        plot_heatmap(embed, name)
