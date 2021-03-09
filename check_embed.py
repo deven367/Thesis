d4c82a0152ff1cf7fa40c987dee2132ac5119327
Deven <masterdeven@gmail.com>
2021-03-09 16:54:41 -0500

    clean new file
    Former-commit-id: 76e4c6c90c3422bafdc620f4debb39200991db5a [formerly c0378764cf8b7b8b48bd43b55d7d7c1a59b8d0ce]
    Former-commit-id: 63e3b55e06e8eb6879aab4945e960bba7260593a

--- check_embed.py
+++ check_embed.py
@@ -1,7 +1,10 @@
 import numpy as np
-from functions import read_json, plot_heatmap
+from functions import read_json, plot_heatmap, get_cleaned_file
 import os
-
+import progressbar
+bar = progressbar.ProgressBar(maxval=20,
+                              widgets=[progressbar.Bar('=', '[', ']'), ' ',
+                              progressbar.Percentage()])
 # embeddings =  np.load('ulysses_infersent_glove.npy')
 # print(embeddings[0].shape)
 
@@ -10,11 +13,15 @@ embeddings = read_json('gg_embeddings.jsonl')
 # print(embeddings.shape)
 # plot_heatmap(embeddings)
 
-embedding_path = './declutr/embeddings/small/poems/'
+embedding_path = './datasets/'
 
-for fx in os.listdir(embedding_path):
-    if fx.endswith('.npy'):
-        name = fx[:-4]
-        print("Loaded "+fx)
-        embed = np.load(embedding_path+fx)
-        plot_heatmap(embed, name)
+# for fx in os.listdir(embedding_path):
+#     if fx.endswith('.npy'):
+#         name = fx[:-4]
+#         print("Loaded "+fx)
+#         embed = np.load(embedding_path+fx)
+#         plot_heatmap(embed, name)
+bar.start()
+fname = 'Republic By Plato.txt'
+get_cleaned_file(embedding_path+fname)
+bar.finish()
