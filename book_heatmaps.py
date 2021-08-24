e48b5d21fe06cbf0d0c563f74bdddcb017237bd7
deven367 <masterdeven@gmail.com>
2021-08-23 22:37:35 -0400

    add imports

---
+++ book_heatmaps.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import pathlib
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics.pairwise import cosine_similarity
+from new_try_dict import *
+from functions import normalize
