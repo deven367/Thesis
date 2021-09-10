9dd151440e7d1d529d466058e240c76bf0ab988f
deven367 <masterdeven@gmail.com>
2021-09-09 22:36:32 -0400

    fix import issue

--- book_heatmaps.py
+++ book_heatmaps.py
@@ -5,5 +5,5 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics.pairwise import cosine_similarity
-from new_try_dict import *
+from make_pkl import *
 from functions import normalize
