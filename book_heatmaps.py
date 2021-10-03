b861d6ec589cc41eb7823375a56bffdb7932ca8a
deven367 <masterdeven@gmail.com>
2021-10-03 17:53:55 -0400

    moving py files to script folder

--- book_heatmaps.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import pathlib
-from pathlib import Path
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics.pairwise import cosine_similarity
-from make_pkl import *
-from functions import normalize
